f1b9bd305ca7dffa56da3964ac698df7f5cde684
BhoomikaR <bhoomikarangegowda@gmail.com>
2025-06-20 22:33:51 +0530

    Add files via upload

---
+++ video54/script.js
@@ -0,0 +1,23 @@
+alert("Hello,World!");
+
+
+console.log("Code is running...");
+console.log("Code is also running...");
+console.log("Code is running like wow...");
+
+var a =prompt("Enter your number");
+var isTrue =prompt("Are you sure you want to leave this page and blast your computer");
+
+if(isTrue){
+    console.log("Computer is blasting");
+}
+
+else{
+    console.log("Computer is blusting");
+
+}
+console.log("Your number is: " + a);
+
+document.title = "Hy I am Pari"
+
+document.body.style.backgroundColor ="lightblue"
