0cc506616239aa077140ba90054332400ef1f600
Bhoomika R <bhoomikarangegowda@gmail.com>
2025-08-02 00:12:34 +0530

    Add files via upload

---
+++ video57/index.js
@@ -0,0 +1,38 @@
+console.log("I am a tutorial on loops")
+
+let a =1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);    
+    
+// }
+
+// let obj={
+//     name:"Harry",
+//     role:"Programmer",
+//     company:"Google"
+// }
+
+// for (const key in obj){
+//         console.log(key)
+//     }
+
+//     for (const c of"Pari"){
+//         console.log(c)
+//     }
+
+// let i = 0;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+
+ let i=10;
+ do{
+    console.log(i)
+    i++;
+ } while(i<6);
