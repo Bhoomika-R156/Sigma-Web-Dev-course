74fcbffc45d1e4d7cc009822b8fa24dcbf6ae7b1
Bhoomika R <bhoomikarangegowda@gmail.com>
2025-08-03 23:25:08 +0530

    Add files via upload

---
+++ video63/loops.js
@@ -0,0 +1,26 @@
+let a = [1,93,5,6,88]
+
+// for (let index = 0; index <a.length; index++) {
+//     const elment = a[index];
+//     console.log(element)
+// }
+
+// a.forEach((value ,index ,arr)=> {
+//     console.log(value,index,arr)
+// })
+
+// let object = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// for (const key in object){
+//     if(object.hasOwnProperty.call(object,key)){
+//         const element = object[key];
+//         console.log(key , element)
+//     }
+// }
+
+for (const value of a){
+    console.log(value)
+}
