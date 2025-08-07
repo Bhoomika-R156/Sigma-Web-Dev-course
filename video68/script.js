24572929f3e5e9be05d4669df29d410934eac873
Bhoomika R <bhoomikarangegowda@gmail.com>
2025-08-07 21:18:49 +0530

    Add files via upload

---
+++ video68/script.js
@@ -0,0 +1,15 @@
+console.log("Pari");
+
+// let bhupendraJogis= document.getElementById("box")
+// console.log(bhupendraJogis)
+
+// boxes[2].style.backgroundColor ="red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColr ="green";
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor=" Blue";
+})
