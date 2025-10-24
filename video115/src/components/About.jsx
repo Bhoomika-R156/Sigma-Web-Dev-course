7eec5d3f43c080dfc8272f0e250a9ce6d74556ff
Bhoomika R <bhoomikarangegowda@gmail.com>
2025-10-24 22:02:24 +0530

    Add files via upload

---
+++ video115/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h1>About</h1>
+      <p>Welcome to the About page! You can add more information here.</p>
+    </section>
+  );
+};
+
+export default About;
